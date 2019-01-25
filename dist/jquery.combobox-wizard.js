d1ae96a2701a86faa50093d7b1ced86783295037
A1Gard <a1gard@4xmen.ir>
2019-01-25 05:52:07 +0330

    show main menu

--- dist/jquery.combobox-wizard.js
+++ dist/jquery.combobox-wizard.js
@@ -1,25 +1,46 @@
-;
-(function ($) {
+;(function ($) {
 
     $.fn.wizcombobox = function (options) {
 
-        $.wizcombobox = this;
+        $.wcb = this;
 
         var settings = $.extend({
-
+            datatree: []
         }, options);
 
 
+        this.init = function (self) {
+
+            // hide element
+            $(self).hide();
+            // set placehodler
+            var placeholder = 'Please select';
+            // check place holder
+            if ($(self).attr('placeholder') != undefined) {
+                placeholder = $(self).attr('placeholder');
+            }
+            $(self).parent().append('<div class="wzcmb">' + placeholder + '</div>');
+
+            $(self).parent().find('.wzcmb').bind('click.open',function () {
+                console.log('x1');
+                if (!$(this).hasClass('active')){
+                    $(this).append('<ul id="wzcmb-list"><li>123</li><li>123</li></ul>');
+                    $("#wzcmb-list").slideDown();
+                    $(this).addClass('active');
+                }else{
+                    $(this).removeClass('active');
+                    $("#wzcmb-list").slideUp(100,function () {
+                      $(this).remove();
+                    })
+                }
+            });
+
+        }
 
         this.each(function () {
-            $(this).hide();
-            var placeholder = 'Please select';
-            if ($(this).attr('placeholder') != undefined){
-                placeholder = $(this).attr('placeholder') ;
-            }
-            $(this).parent().append('<div class="wzcmb">'+placeholder+'</div>');
-        });
 
+            $.wcb.init(this);
+        });
 
 
         return this;
