a02489aa3e7d5b683604cbd5a80870d20fff53f3
A1Gard <a1gard@4xmen.ir>
2019-01-25 05:10:47 +0330

    init files

---
+++ dist/jquery.combobox-wizard.js
@@ -0,0 +1,24 @@
+;
+(function ($) {
+
+    $.fn.wizcombobox = function (options) {
+
+        $.wizcombobox = this;
+
+        var settings = $.extend({
+
+        }, options);
+
+
+
+        this.each(function () {
+
+        });
+
+
+
+        return this;
+
+    };
+
+}(jQuery));
