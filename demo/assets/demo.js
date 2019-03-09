c4e51a96690eb35008f79e5e1904ada74a17b550
A1Gard <a1gard@4xmen.ir>
2019-03-09 16:29:11 +0330

    optimize demo

--- demo/assets/demo.js
+++ demo/assets/demo.js
@@ -1,70 +1,86 @@
-$(function () {
-    $("#test").treeSelect({
-        mainTitle: 'x main cat',
-        datatree: [{
-            title: 'text1',
-            value: 1,
-            child: []
-        }, {
-            title: 'text2',
-            value: 2,
-            child: []
-        },
+var defJson = [
+    {
+    title: 'text1',
+    value: 1,
+    child: []
+}, {
+    title: 'text2',
+    value: 2,
+},
+    {
+        title: 'text3',
+        value: 3,
+        child: [
             {
-                title: 'text3',
+                title: 'lv2 text3',
                 value: 3,
+                child: []
+            },
+            {
+                title: 'lv2 text4',
+                value: 4,
                 child: [
                     {
-                        title: 'lv2 text3',
+                        title: 'lv3 text3',
                         value: 3,
                         child: []
                     },
                     {
-                        title: 'lv2 text4',
+                        title: 'lv3 text4',
                         value: 4,
-                        child: [
-                            {
-                                title: 'lv3 text3',
-                                value: 3,
-                                child: []
-                            },
-                            {
-                                title: 'lv3 text4',
-                                value: 4,
-                                child: []
-                            },
-                            {
-                                title: 'lv3 text5',
-                                value: 5,
-                                child: []
-                            },
-                        ]
+                        child: []
                     },
                     {
-                        title: 'lv2 text5',
+                        title: 'lv3 text5',
                         value: 5,
                         child: []
-                    }
+                    },
                 ]
             },
             {
-                title: 'text4',
-                value: 4,
-                child: []
-            },
-            {
-                title: 'text5',
+                title: 'lv2 text5',
                 value: 5,
                 child: []
-            },
+            }
         ]
+    },
+    {
+        title: 'text4',
+        value: 4,
+        child: []
+    },
+    {
+        title: 'text5',
+        value: 5,
+        child: []
+    },
+];
+
+$(function () {
+
+    $("h3").click(function () {
+        $($(this).data('id')).slideToggle(500);
+    });
+    $("#test").treeSelect({
+        datatree: defJson
     });
 
-    $("#test2").treeSelect({
-        mainTitle: 'mCa00000t',
+
+    $("#demo2").treeSelect({
+        mainTitle: 'سرفصل های اصلی',
         direction: 'rtl',
+        datatree: defJson
+    });
+
+    $("#demo3").treeSelect({
         transition: 'slide',
         selectablePrernt: true,
+        datatree: defJson
+    });
+
+    $("#demo4").treeSelect({
+        datatree: defJson,
+        transition: 'none',
         onOpen: function () {
             console.log('onOpen event triggered');
         }, onClose: function () {
@@ -74,7 +90,10 @@ $(function () {
             console.log(sel);
         }, OnChange: function (oldv, newv) {
             console.log('OnChange event triggered old: ', oldv, 'new:', newv);
-        },
+        }
+    });
+
+    $("#demo5").treeSelect({
         json: {
             title: 't',
             value: 'v',
