3dfe3b6ea248f6ca82090ac59ca97fbbec64e0fd
A1Gard <a1gard@4xmen.ir>
2019-06-22 11:04:41 +0430

    change trs to xts

--- dist/x-tree-select.js
+++ dist/x-tree-select.js
@@ -17,14 +17,14 @@
         };
 
         // set default store for setting
-        if ($.trsStore === undefined) {
+        if ($.xtsStore === undefined) {
             $.scbCounter = -1;
-            $.trsStore = [];
+            $.xtsStore = [];
             $.currentCounter = 0;
         }
 
         // make app var for use in plugin
-        $.trs = this;
+        $.xts = this;
 
         // change plugin info
         var settings = $.extend({
@@ -63,9 +63,9 @@
 
         // store setting for use
         $.scbCounter++;
-        $.trsStore[$.scbCounter] = settings;
+        $.xtsStore[$.scbCounter] = settings;
 
-        // console.log($.trsStore);
+        // console.log($.xtsStore);
 
         /**
          * initial element
@@ -85,63 +85,63 @@
 
             // set rtl class if is rtl
             var rtlClass = '';
-            if ($.trsStore[$.scbCounter].direction === 'rtl') {
-                rtlClass = 'trs-rtl';
+            if ($.xtsStore[$.scbCounter].direction === 'rtl') {
+                rtlClass = 'xts-rtl';
             }
 
             // init element for click for choose
-            $(self).parent().append('<div class="trsel ' + rtlClass + '" data-trcounter="' +
+            $(self).parent().append('<div class="xtsel ' + rtlClass + '" data-trcounter="' +
                 $.scbCounter + '" >' + placeholder + '</div>');
 
 
             // initial idx for use in naviagion
-            settings.datatree = $.trs.makeId('1', settings.datatree);
+            settings.datatree = $.xts.makeId('1', settings.datatree);
             // on click combowizard
-            $(self).parent().find('.trsel').bind('click.open', function (e) {
+            $(self).parent().find('.xtsel').bind('click.open', function (e) {
                 if (e.target !== this) {
                     return;
                 }
 
-                var trsel = $(this);
+                var xtsel = $(this);
 
-                if (trsel.hasClass("loading")) {
+                if (xtsel.hasClass("loading")) {
                     return;
                 }
 
-                trsel.addClass("loading");
+                xtsel.addClass("loading");
                 setTimeout(function () {
-                    trsel.removeClass("loading");
+                    xtsel.removeClass("loading");
                 }, 600);
 
                 $.currentCounter = $(this).data('trcounter');
-                $.lastx.title = $.trsStore[$.currentCounter].mainTitle;
+                $.lastx.title = $.xtsStore[$.currentCounter].mainTitle;
                 // check is open list
                 if (!$(this).hasClass('active')) {
 
-                    $.trsStore[$.currentCounter].onOpen();
+                    $.xtsStore[$.currentCounter].onOpen();
                     // find target for last value
-                    $.trs.target = $(this).parent().find('input');
+                    $.xts.target = $(this).parent().find('input');
                     // find select text position
-                    $.trs.text = $(self).parent().find('.trsel');
+                    $.xts.text = $(self).parent().find('.xtsel');
                     //if now list show list and countiniu
-                    $(this).append('<ul id="trsel-list"></ul>');
-                    if ($.trsStore[$.currentCounter].navx === undefined) {
+                    $(this).append('<ul id="xtsel-list"></ul>');
+                    if ($.xtsStore[$.currentCounter].navx === undefined) {
                         // show first list main cat in list
-                        $.trs.showTree($.trsStore[$.currentCounter].datatree);
+                        $.xts.showTree($.xtsStore[$.currentCounter].datatree);
                     } else {
-                        $.lastx = $.trsStore[$.currentCounter].lastx;
-                        $.navigatex = $.trsStore[$.currentCounter].navx;
-                        $.trs.showTree($.trs.findTree($.trsStore[$.currentCounter].datatree, $.trsStore[$.currentCounter].lastx.id));
+                        $.lastx = $.xtsStore[$.currentCounter].lastx;
+                        $.navigatex = $.xtsStore[$.currentCounter].navx;
+                        $.xts.showTree($.xts.findTree($.xtsStore[$.currentCounter].datatree, $.xtsStore[$.currentCounter].lastx.id));
                     }
                     // slide down list
-                    $("#trsel-list").slideDown(function () {
+                    $("#xtsel-list").slideDown(function () {
                         $(document).bind('click.handvarrsl', function (e) {
-                            if (!$(e.target).is(".trsel-childer, .trsel-back")) {
-                                $.trs.resetClose();
-                                $("#trsel-list").slideUp(200, function () {
+                            if (!$(e.target).is(".xtsel-childer, .xtsel-back")) {
+                                $.xts.resetClose();
+                                $("#xtsel-list").slideUp(200, function () {
                                     $(this).remove();
                                 });
-                                $($.trs.text).removeClass('active');
+                                $($.xts.text).removeClass('active');
                             }
                             e.preventDefault();
                             return false;
@@ -157,19 +157,19 @@
 
                     // hide and remove list
                     $(this).removeClass('active');
-                    $("#trsel-list").slideUp(100, function () {
-                        // $.trs.resetClose();
+                    $("#xtsel-list").slideUp(100, function () {
+                        // $.xts.resetClose();
                         $(this).remove();
                     });
                 }
             });
 
             //reset
-            $(document).off('click', '#trsel-list li');
+            $(document).off('click', '#xtsel-list li');
             // on click items
-            $(document).on('click', '#trsel-list li', function (e) {
+            $(document).on('click', '#xtsel-list li', function (e) {
                 // has child & no parent selecatble
-                if ($(this).hasClass('trsel-childer') && !$(e.target).hasClass('trsel-selectable')) {
+                if ($(this).hasClass('xtsel-childer') && !$(e.target).hasClass('xtsel-selectable')) {
                     // make navigation
                     var tmp = {
                         title: $.lastx.title,
@@ -181,49 +181,49 @@
 
 
                     // transtion ;
-                    $("#trsel-list").addClass($.treeselect_animation[$.trsStore[$.currentCounter].transition]);
+                    $("#xtsel-list").addClass($.treeselect_animation[$.xtsStore[$.currentCounter].transition]);
 
 
                     var xself = this;
                     var ax = setTimeout(function () {
                         // show child items
-                        $.trs.showTree($.trs.findTree($.trsStore[$.currentCounter].datatree, $(xself).data('id')));
+                        $.xts.showTree($.xts.findTree($.xtsStore[$.currentCounter].datatree, $(xself).data('id')));
                         // remove effect
-                        $("#trsel-list").removeClass($.treeselect_animation[$.trsStore[$.currentCounter].transition]);
+                        $("#xtsel-list").removeClass($.treeselect_animation[$.xtsStore[$.currentCounter].transition]);
                     }, 600);
 
-                } else if ($(this).hasClass('trsel-back')) { // if click on back
+                } else if ($(this).hasClass('xtsel-back')) { // if click on back
                     if ($.navigatex[$.navigatex.length - 1] !== undefined) {
                         // roll back navigtaion to last
                         $.lastx.title = $.navigatex[$.navigatex.length - 1].title;
                         $.lastx.id = $.navigatex[$.navigatex.length - 1].id;
                     } else {
                         // check is navigation emoppty set def
-                        $.lastx.title = $.trsStore[$.currentCounter].mainTitle;
+                        $.lastx.title = $.xtsStore[$.currentCounter].mainTitle;
                         $.lastx.id = '';
                     }
                     // pop last navigation item
                     $.navigatex.pop();
                     // get parent list
-                    var lst = $.trs.findTree($.trsStore[$.currentCounter].datatree, $(this).data('id'));
+                    var lst = $.xts.findTree($.xtsStore[$.currentCounter].datatree, $(this).data('id'));
                     // if has not parent show main cat
-                    $("#trsel-list").addClass($.treeselect_animation[$.trsStore[$.currentCounter].transition]);
+                    $("#xtsel-list").addClass($.treeselect_animation[$.xtsStore[$.currentCounter].transition]);
 
                     var ax = setTimeout(function () {
                         if (lst.length === 0) {
-                            $.trs.showTree($.trsStore[$.currentCounter].datatree);
+                            $.xts.showTree($.xtsStore[$.currentCounter].datatree);
                         } else {
                             // then show parent list
-                            $.trs.showTree(lst);
+                            $.xts.showTree(lst);
                         }
-                        $("#trsel-list").removeClass($.treeselect_animation[$.trsStore[$.currentCounter].transition]);
+                        $("#xtsel-list").removeClass($.treeselect_animation[$.xtsStore[$.currentCounter].transition]);
                     }, 600);
 
                 } else { // choose|select value
                     // onChange event
-                    $.trsStore[$.currentCounter].onChange({
-                        value: $($.trs.target).val(),
-                        text: $($.trs.text).clone()    //clone the element
+                    $.xtsStore[$.currentCounter].onChange({
+                        value: $($.xts.target).val(),
+                        text: $($.xts.text).clone()    //clone the element
                             .children() //select all the children
                             .remove()   //remove all the children
                             .end()  //again go back to selected element
@@ -233,28 +233,28 @@
                         text: $(this).text()
                     });
                     // set main input value
-                    $($.trs.target).val($(this).data('value'));
+                    $($.xts.target).val($(this).data('value'));
                     // set choosed test
-                    $($.trs.text).text($(this).text());
+                    $($.xts.text).text($(this).text());
                     // if selected is not first level store level & last titles
                     if ($.navigatex.length > 0) {
-                        $.trsStore[$.currentCounter].navx = $.navigatex;
-                        $.trsStore[$.currentCounter].lastx = $.lastx;
+                        $.xtsStore[$.currentCounter].navx = $.navigatex;
+                        $.xtsStore[$.currentCounter].lastx = $.lastx;
                     } else {
-                        delete $.trsStore[$.currentCounter].navx;
-                        delete $.trsStore[$.currentCounter].lastx;
+                        delete $.xtsStore[$.currentCounter].navx;
+                        delete $.xtsStore[$.currentCounter].lastx;
                     }
                     // remove active class and hide list
-                    $($.trs.text).removeClass('active');
+                    $($.xts.text).removeClass('active');
                     // onChange
-                    $.trsStore[$.currentCounter].onSelect({
+                    $.xtsStore[$.currentCounter].onSelect({
                         value: $(this).data('value'),
                         text: $(this).text(),
                         id: $(this).data('id'),
                         parent: $.lastx,
                         ancestors: $.navigatex
                     });
-                    $("#trsel-list").slideUp(100, function () {
+                    $("#xtsel-list").slideUp(100, function () {
                         $(this).remove();
                     });
                 }
@@ -280,13 +280,13 @@
                 var prefix = base + '-' + (parseInt(ix) + 1);
                 itm.idx = prefix;
                 //if there is no child object, just create it
-                if (itm[$.trsStore[$.scbCounter].json.child] === undefined) {
-                    itm[$.trsStore[$.scbCounter].json.child] = [];
+                if (itm[$.xtsStore[$.scbCounter].json.child] === undefined) {
+                    itm[$.xtsStore[$.scbCounter].json.child] = [];
                 }
                 // if has child
-                if (itm[$.trsStore[$.scbCounter].json.child].length !== 0) {
+                if (itm[$.xtsStore[$.scbCounter].json.child].length !== 0) {
                     // recall this function for children
-                    itm[$.trsStore[$.scbCounter].json.child] = $.trs.makeId(prefix, itm[$.trsStore[$.scbCounter].json.child]);
+                    itm[$.xtsStore[$.scbCounter].json.child] = $.xts.makeId(prefix, itm[$.xtsStore[$.scbCounter].json.child]);
                 }
                 all.push(itm);
             }
@@ -301,34 +301,34 @@
         this.showTree = function (list, cb) {
             var content = '';
             // clear li list
-            $("#trsel-list li").remove();
+            $("#xtsel-list li").remove();
             // back button handle
             // has parent need back button
             if ($.navigatex.length !== 0) {
                 // if navigation is empity not need back button
                 var back = $.navigatex[$.navigatex.length - 1];
 
-                content += '<li class="trsel-back" data-id="' + back.id + '"> &nbsp;' + back.title + '</li>';
+                content += '<li class="xtsel-back" data-id="' + back.id + '"> &nbsp;' + back.title + '</li>';
             }
             // show list passed to function
             for (var ix in list) {
                 var item = list[ix];
                 // ad childs
                 // check has child
-                var clsx = ' class="trsel-childer"';
+                var clsx = ' class="xtsel-childer"';
                 var select = '';
-                if (item[$.trsStore[$.currentCounter].json.child].length === 0) {
+                if (item[$.xtsStore[$.currentCounter].json.child].length === 0) {
                     clsx = ' class=""';
                 } else {
-                    if ($.trsStore[$.currentCounter].selectablePrernt) {
-                        var select = '<span class="trsel-selectable"></span>';
+                    if ($.xtsStore[$.currentCounter].selectablePrernt) {
+                        var select = '<span class="xtsel-selectable"></span>';
                     }
                 }
 
                 // li to list
-                content += '<li' + clsx + ' data-id="' + item.idx + '"  data-value="' + item[$.trsStore[$.currentCounter].json.value] + '">' + select + item[$.trsStore[$.currentCounter].json.title] + '</li>';
+                content += '<li' + clsx + ' data-id="' + item.idx + '"  data-value="' + item[$.xtsStore[$.currentCounter].json.value] + '">' + select + item[$.xtsStore[$.currentCounter].json.title] + '</li>';
             }
-            $("#trsel-list").html(content);
+            $("#xtsel-list").html(content);
             if (cb !== undefined) {
                 cb();
             }
@@ -347,14 +347,14 @@
             for (var ix in list) {
                 var item = list[ix];
                 if (item.idx === idx) {
-                    return item[$.trsStore[$.currentCounter].json.child];
+                    return item[$.xtsStore[$.currentCounter].json.child];
                 }
             }
             // if not fond in first level each the childs
             for (var ix in list) {
                 var item = list[ix];
-                if (item[$.trsStore[$.currentCounter].json.child].length !== 0) {
-                    var tmp = $.trs.findTree(item[$.trsStore[$.currentCounter].json.child], idx);
+                if (item[$.xtsStore[$.currentCounter].json.child].length !== 0) {
+                    var tmp = $.xts.findTree(item[$.xtsStore[$.currentCounter].json.child], idx);
                     // if found idx retrur
                     if (tmp.length !== 0) {
                         return tmp;
@@ -380,35 +380,35 @@
             if (parent != null) {
                 $.navigatex.push({
                     id: parent.idx,
-                    title: parent[$.trsStore[c].json.title]
+                    title: parent[$.xtsStore[c].json.title]
                 });
             } else {
                 $.navigatex.push({
                     id: "",
-                    title:  $.trsStore[c].mainTitle
+                    title:  $.xtsStore[c].mainTitle
                 });
-                $.lastx.title = $.trsStore[c].mainTitle;
+                $.lastx.title = $.xtsStore[c].mainTitle;
                 $.lastx.id = '';
             }
             // each all
             for (var ix in list) {
                 var item = list[ix];
-                if (item[$.trsStore[c].json.value] == val) {
+                if (item[$.xtsStore[c].json.value] == val) {
                     return item;
                 }
                 // if has child
-                if (item[$.trsStore[c].json.child] != undefined && item[$.trsStore[c].json.child].length > 0) {
+                if (item[$.xtsStore[c].json.child] != undefined && item[$.xtsStore[c].json.child].length > 0) {
                     // add last
                     $.lastx = {
                         id: item.idx,
-                        title: item[$.trsStore[c].json.title]
+                        title: item[$.xtsStore[c].json.title]
                     };
-                    var back = this.findItem(item[$.trsStore[c].json.child], val, c, item);
+                    var back = this.findItem(item[$.xtsStore[c].json.child], val, c, item);
                     if (back != false) {
                         return back;
                     }
                 }
-                // console.log($.trsStore[c].json.child);
+                // console.log($.xtsStore[c].json.child);
             }
 
             $.navigatex.pop();
@@ -423,13 +423,13 @@
          */
         this.setDefault = function (c, val) {
             // find value
-            var back = $.trs.findItem($.trsStore[c].datatree, val, c);
+            var back = $.xts.findItem($.xtsStore[c].datatree, val, c);
             // if is exits val
             if (back != false) {
-                $.trsStore[c].lastx = $.lastx;
+                $.xtsStore[c].lastx = $.lastx;
                 $.navigatex.pop();
-                $.trsStore[c].navx = $.navigatex;
-                $("[data-trcounter='" + c + "']").text(back[$.trsStore[c].json.title]);
+                $.xtsStore[c].navx = $.navigatex;
+                $("[data-trcounter='" + c + "']").text(back[$.xtsStore[c].json.title]);
             }
         };
 
@@ -437,11 +437,11 @@
          * reset values on close
          */
         this.resetClose = function () {
-            $.trsStore[$.currentCounter].onClose();
+            $.xtsStore[$.currentCounter].onClose();
             // reset navagtion value
             $.navigatex = [];
             $.lastx = {
-                title: $.trsStore[$.currentCounter].mainTitle,
+                title: $.xtsStore[$.currentCounter].mainTitle,
                 id: ''
             };
             // reset docuemnt bind
@@ -452,7 +452,7 @@
             this.html(currentInnerText + " " + newValue)
         };
         this.each(function () {
-            $.trs.init(this);
+            $.xts.init(this);
         });
         return this;
 
